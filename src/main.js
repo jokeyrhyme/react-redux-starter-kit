7733bc58bc33e594a8ee538db1ca7369c731fb4a
Ron Waldon <jokeyrhyme@gmail.com>
2016-05-08 11:54:44 +1000

    WIP

--- src/main.js
+++ src/main.js
@@ -1,37 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import bootstrap from 'redux-bootstrap'
 import createBrowserHistory from 'history/lib/createBrowserHistory'
-import { Router, useRouterHistory } from 'react-router'
-import { syncHistoryWithStore } from 'react-router-redux'
 import createStore from './store/createStore'
-import { Provider } from 'react-redux'
 
 const MOUNT_ELEMENT = document.getElementById('root')
 
-// Configure history for react-router
-const browserHistory = useRouterHistory(createBrowserHistory)({
-  basename: __BASENAME__
-})
-
-// Create redux store and sync with react-router-redux. We have installed the
-// react-router-redux reducer under the key "router" in src/routes/index.js,
-// so we need to provide a custom `selectLocationState` to inform
-// react-router-redux of its location.
-const store = createStore(window.__INITIAL_STATE__, browserHistory)
-const history = syncHistoryWithStore(browserHistory, store, {
-  selectLocationState: (state) => state.router
-})
+const { history, root, store } = bootstrap({
+  container: MOUNT_ELEMENT,
+  createHistory: createBrowserHistory,
+  historyOptions: {
+    basename: __BASENAME__
+  },
+  initialState: window.__INITIAL_STATE__,
+  routes
+});
 
 let render = (key = null) => {
-  const routes = require('./routes/index').default(store)
-  const App = (
-    <Provider store={store}>
-      <div style={{ height: '100%' }}>
-        <Router history={history} children={routes} key={key} />
-      </div>
-    </Provider>
-  )
-  ReactDOM.render(App, MOUNT_ELEMENT)
+  ReactDOM.render(root, MOUNT_ELEMENT)
 }
 
 // Enable HMR and catch runtime errors in RedBox
@@ -41,7 +27,7 @@ if (__DEV__ && module.hot) {
   const renderError = (error) => {
     const RedBox = require('redbox-react')
 
-    ReactDOM.render(<RedBox error={error}/>, MOUNT_ELEMENT)
+    ReactDOM.render(<RedBox error={error} />, MOUNT_ELEMENT)
   }
   render = () => {
     try {
